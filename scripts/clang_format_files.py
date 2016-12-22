8e81080f560636e23053f0311d22c0414476de22
James Moran <jojo.2nd.jm@gmail.com>
2016-12-22 20:02:18 +0000

    script for running clang format on game source.

---
+++ scripts/clang_format_files.py
@@ -0,0 +1,42 @@
+## clang_format_files
+import argparse
+import zipfile
+import os
+import hashlib
+import json
+from os.path import join, realpath, splitext, relpath
+from subprocess import Popen, PIPE
+import time
+
+
+parser = argparse.ArgumentParser(prog='binary asset packager',description='Script to package binary data from a git repro into a deploy-able zip')
+parser.add_argument('-d','--directory', action='append', help='Source directory to search. Recursive.')
+#parser.add_argument('-s','--style', help='filepath of clang format style.')
+
+file_types = [
+    '.c',
+    '.cpp',
+    '.h',
+    '.hpp',
+    '.cxx',
+]
+
+def main():
+    args = parser.parse_args()
+    directories = [realpath(x) for x in args.directory]
+    print "Checking files in %s"%(str([realpath(x) for x in directories]))
+
+
+    source_files = []
+    for d in directories:
+        for root, dirs, files in os.walk(d):
+            source_files += [ realpath(join(root, x)) for x in files if splitext(x)[1] in file_types]
+
+	cmd = ['../external/LLVM/clang-format', '-i', '-style=file'] 
+	cmd += source_files
+	#print "Running command line", cmd
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE)
+    returncode = p.wait()
+
+if __name__ == '__main__':
+    main()
